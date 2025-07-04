5c53af26191af256a1fa024555822cb9d7a4bd93
= <=>
2025-07-04 22:15:39 +0530

    backend

--- server.js
+++ server.js
@@ -14,12 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect("mongodb+srv://nisha:nisha2007@cluster0.1stireb.mongodb.net/Formdata", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+mongoose.connect("mongodb+srv://nisha:nisha2007@cluster0.1stireb.mongodb.net/Formdata")
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
 
 
 
